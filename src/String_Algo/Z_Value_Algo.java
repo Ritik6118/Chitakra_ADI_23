7d5512f93a7409020e64243a4150509b4964addc
unknown <ritik6118a@gmail.com>
2023-12-08 17:48:25 +0530

    Class Room Codes

---
+++ src/String_Algo/Z_Value_Algo.java
@@ -0,0 +1,45 @@
+package String_Algo;
+
+import java.util.Arrays;
+
+public class Z_Value_Algo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="ababacababad";
+		String p="ab";
+		int[] z=z_fun(p,s);
+		int c=0;
+		System.out.println(Arrays.toString(z));
+		for(int i=0;i<z.length;i++) {
+			if(z[i]==p.length()) {
+				c++;
+				System.out.println(i-p.length()-1);
+			}
+		}
+		System.out.println(c);
+	}
+
+	private static int[] z_fun(String p, String s) {
+		// TODO Auto-generated method stub
+		String st=p+"$"+s;
+		int l=0;
+		int r=0;
+		int[]z=new int[st.length()];
+		for(int i=1;i<st.length();i++) {
+			if(i<r) {
+				z[i]=Math.min(z[i-l], r-i+1);
+			}
+			while(st.charAt(z[i])==st.charAt(i+z[i])){
+				z[i]++;
+			}
+			if(i+z[i]-1>r) {
+				l=i;
+				r=i+z[i]-1;
+			}
+		}
+		return z;
+	}
+	
+
+}
